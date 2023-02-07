24e2f1bbed42f6170364fe6a99d2381cb1e6b2e8
root2865 <ethicalhacker2580@gmail.com>
2023-02-07 23:36:39 +0530

    optimized the code for data input for all condition

--- Bank_FD_management/Bank_FD_management/frmcreatecustomer.cs
+++ Bank_FD_management/Bank_FD_management/frmcreatecustomer.cs
@@ -239,17 +239,33 @@ namespace Bank_FD_management
 
         private void btnsave_Click(object sender, EventArgs e)
         {
-            try
-            {
-                setConnection();
-                OleDbCommand cmd = new OleDbCommand("insert into customer_master (C_name , address , city , state , district , pin_code , phone , e_mail , DOB  , PAN , Add_date , is_minor_cust , g_name , g_addr , g_relation , g_phone , N_name , Rel_of_nom , Phone_nom , Is_minor , Age_minor) values('" + txtname.Text + "', '" + txtaddress.Text + "', '" + cmbcity.Text + "', '" + cmbstate.Text + "', '" + cmbdistrict.Text + "', " + txtPinCode.Text + ", " + txtPhone.Text + ",'" + txtemail.Text+ "', #" +dtpcustbirth.Value +"# ,'" + txtpan.Text + "', #" + DateTime.Now + "#, " + chbminor.Checked + ", '" + txtgname.Text + "', '" + txtgaddress.Text + "', '" + txtgrelation.Text + "', " + txtGuardPhone.Text + ", '" + txtnname.Text + "', '" + txtnrelation.Text + "', " + txtNomPhone.Text + ", " + chbisnomminor.Checked + ", " + (DateTime.Now.Year - dobNom.Value.Year) +")", conn);
-                cmd.ExecuteNonQuery();
-            }
+            //try
+            //{
+                if ((chbminor.Checked) && (chbnominee.Checked))
+                {
+                    setConnection();
+                    OleDbCommand cmd = new OleDbCommand("insert into customer_master (C_name , address , city , state , district , pin_code , phone , e_mail , DOB  , PAN , Add_date , is_minor_cust , g_name , g_addr , g_relation , g_phone , N_name , Rel_of_nom , Phone_nom , Is_minor , Age_minor) values('" + txtname.Text + "', '" + txtaddress.Text + "', '" + cmbcity.Text + "', '" + cmbstate.Text + "', '" + cmbdistrict.Text + "', " + txtPinCode.Text + ", " + txtPhone.Text + ",'" + txtemail.Text + "', #" + dtpcustbirth.Value + "# ,'" + txtpan.Text + "', #" + DateTime.Now + "#, " + chbminor.Checked + ", '" + txtgname.Text + "', '" + txtgaddress.Text + "', '" + txtgrelation.Text + "', " + txtGuardPhone.Text + ", '" + txtnname.Text + "', '" + txtnrelation.Text + "', " + txtNomPhone.Text + ", " + chbisnomminor.Checked + ", " + (DateTime.Now.Year - dobNom.Value.Year) + ")", conn);
+                    cmd.ExecuteNonQuery();
+                }
+                else if (chbminor.Checked)
+                {
+                    setConnection();
+                    OleDbCommand cmd = new OleDbCommand("insert into customer_master (C_name , address , city , state , district , pin_code , phone , e_mail , DOB  , PAN , Add_date , is_minor_cust , g_name , g_addr , g_relation , g_phone )values('" + txtname.Text + "', '" + txtaddress.Text + "', '" + cmbcity.Text + "', '" + cmbstate.Text + "', '" + cmbdistrict.Text + "', " + txtPinCode.Text + ", " + txtPhone.Text + ",'" + txtemail.Text + "', #" + dtpcustbirth.Value + "# ,'" + txtpan.Text + "', #" + DateTime.Now + "#, " + chbminor.Checked + ", '" + txtgname.Text + "', '" + txtgaddress.Text + "', '" + txtgrelation.Text + "', " + txtGuardPhone.Text + ")", conn);
+                     cmd.ExecuteNonQuery();
+                }
+                else if(chbnominee.Checked)
+                {
+                    setConnection();
+                    OleDbCommand cmd = new OleDbCommand("insert into customer_master (C_name , address , city , state , district , pin_code , phone , e_mail , DOB  , PAN , Add_date ,N_name , Rel_of_nom , Phone_nom , Is_minor , Age_minor) values('" + txtname.Text + "', '" + txtaddress.Text + "', '" + cmbcity.Text + "', '" + cmbstate.Text + "', '" + cmbdistrict.Text + "', " + txtPinCode.Text + ", " + txtPhone.Text + ",'" + txtemail.Text + "', #" + dtpcustbirth.Value + "# ,'" + txtpan.Text + "', #" + DateTime.Now + "#,'" + txtnname.Text + "', '" + txtnrelation.Text + "', " + txtNomPhone.Text + ", " + chbisnomminor.Checked + ", " + (DateTime.Now.Year - dobNom.Value.Year) + ")", conn);
+                    cmd.ExecuteNonQuery();
+                }
 
-            catch (OleDbException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+           // }
+
+            //catch (OleDbException ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
 
             MessageBox.Show("data inserted!");
             conn.Close();
